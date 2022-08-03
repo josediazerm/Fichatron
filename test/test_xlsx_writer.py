f3d0f8807df35767798546e11165c5e0fa648227
unknown <josemariadiazdeharo@gmail.com>
2022-08-03 12:15:16 +0200

    Updated tests

--- test/test_xlsx_writer.py
+++ test/test_xlsx_writer.py
@@ -19,6 +19,18 @@ class Test(unittest.TestCase):
         template_file, template_sheet = xlsx_writer.load_template()
         xlsx_writer.save_file(template_file)
         template_route = xlsx_writer.obtain_template_location()
+        test_passed = False
         if os.path.exists(f"{template_route}//written-excel.xlsx"):
+            test_passed = True
             os.remove(f"{template_route}//written-excel.xlsx")
+        self.assertTrue(test_passed)
+
+    def test_write_value(self):
+        template_file, template_sheet = xlsx_writer.load_template()
+        test_value = 10
+        xlsx_writer.write_value_in_determined_table_row_column(value=test_value,
+                                                               table=template_sheet,
+                                                               row=1,
+                                                               column="A")
+        self.assertEqual(template_sheet["A1"].value, test_value)
 
